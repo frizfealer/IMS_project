cad89ac91254db71e9ecc25091a20ac23b0af838
YC-HARN <ycharn@cs.unc.edu>
2014-08-14 09:11:02 -0400

    update analze results
    With the option to only output significant dictionary

--- dictioanry_learning_analysis/analyzeResults.m
+++ dictioanry_learning_analysis/analyzeResults.m
@@ -1,4 +1,4 @@
-function [ sigDict, sigM] = analyzeResults( expRec, DTemplate, BlkDS, SpeciesM, DIonName, mzAxis, requireInfoPath, thresDiffW, outNum, outFolder )
+function [ sigDict, sigM] = analyzeResults( expRec, DTemplate, BlkDS, SpeciesM, DIonName, mzAxis, requireInfoPath, thresDiffW, outNum, outFolder, outPicFlag )
 %UNTITLED3 Summary of this function goes here
 % requireInfoPath: a data structure with three field: 
 % 1. bioPath: for bio file path, 2. macPath: for machine file path, 3. 
@@ -41,79 +41,89 @@ end
 [~,idxIns2] = sort(ins2,'descend');
 idxIns2=idxIns2(1:outNum)';
 
-% bioFilePath = 'D:\Users\YeuChern\GitHub\IMS_project\example\realExperiment_100831_113_333_136_26hr_0_1XLB_2\bioPic002.jpg';
-% macFilePath = 'D:\Users\YeuChern\GitHub\IMS_project\example\realExperiment_100831_113_333_136_26hr_0_1XLB_2\macPic_2.jpg';
-bioFilePath = requireInfoPath.bioPath;
-macFilePath = requireInfoPath.macPath;
-MSFilePath = 'MSFile.jpg';
-mapMSFilePath = 'mapMSFile.jpg';
-LineStyleTable={'-', '--',':' };
-ColorSpecTable=get(gca,'ColorOrder') ; ColorSpecTable(end-1, :) = [0 0 0]; ColorSpecTable(end, :)=[1 1 0];
-MarkerTable={'+','o','x','*', 's', 'd'};
-sigDict= zeros( length(mzAxis), outNum);
-cntSigDict = 1;
-sigM = zeros( outNum, 1 );
-for j = idxIns2
-%     if ins2(j) < thresDiffW
-%         continue;
-%     end
-    fprintf( 'output MW: %g\n', SpeciesM( targetW(j) ) );
-    sigM(cntSigDict) = SpeciesM( targetW(j) ) ;
-    figure('Visible','Off'); 
-    imagesc( reshape( outW(targetW(j),:), IHEI, IWID ) ); axis off; export_fig(MSFilePath);
-    [~, ~, ~, ~ ] = image_registered( bioFilePath, macFilePath, MSFilePath, requireInfoPath.registeredPath, 0.2, 0 );
-    export_fig(mapMSFilePath);
-    delete(MSFilePath);
-    figure('Visible','Off');
-    h1 = subplot(1,2,1);
-    possIdx = find(DTemplate(:,targetW(j))~=0);
-    mzRange = (mzAxis(possIdx(1))-10):(mzAxis(possIdx(end))+10);
-    h=gca;
-    plot(h, mzRange,zeros(length(mzRange), 1));
-    ylim([0 1.1]);
-    lineHan = [];
-    textCell=[];
-    cnt = 1;
-    sigDict(:, cntSigDict) = outD(:, targetW(j) ); 
-    cntSigDict = cntSigDict + 1;
-    for i = 1:length(possIdx)
-        cColor = ColorSpecTable( mod(i, 7)+1,: );
-        cLine = LineStyleTable{ mod(i, 3)+1 };
-        if outD(possIdx(i), targetW(j) ) >=1e-2
-            lineHan(cnt) = ...
-                line( [mzAxis(possIdx(i)) mzAxis(possIdx(i))], [0 outD(possIdx(i), targetW(j) )], 'LineWidth',2,  'LineStyle', cLine, 'Color', cColor);
-            textCell{cnt} = strcat('[', deblank(DIonName{targetW(j)}{i,:}) ,']: ', num2str(mzAxis(possIdx(i))) );
-            cnt = cnt + 1;
-%         if outD(possIdx(i), targetW(j) ) >=1e-2
-%             annoText = strcat('[', deblank(DIonName{targetW(j)}(i,:)) ,']');
-%             if outD(possIdx(i), targetW(j) ) >= 0.6
-%                 dist = sqrt(length(annoText)^2/2)*0.5;
-%                 text( mzAxis(possIdx(i))-3, outD(possIdx(i), targetW(j))-dist, annoText, 'FontSize', 9, 'Rotation',90);
-%             else
-%                 text( mzAxis(possIdx(i)), outD(possIdx(i), targetW(j)), annoText, 'FontSize', 9, 'Rotation', 90);
-%             end
-%         end
+if outPicFlag == 1
+    % bioFilePath = 'D:\Users\YeuChern\GitHub\IMS_project\example\realExperiment_100831_113_333_136_26hr_0_1XLB_2\bioPic002.jpg';
+    % macFilePath = 'D:\Users\YeuChern\GitHub\IMS_project\example\realExperiment_100831_113_333_136_26hr_0_1XLB_2\macPic_2.jpg';
+    bioFilePath = requireInfoPath.bioPath;
+    macFilePath = requireInfoPath.macPath;
+    MSFilePath = 'MSFile.jpg';
+    mapMSFilePath = 'mapMSFile.jpg';
+    LineStyleTable={'-', '--',':' };
+    ColorSpecTable=get(gca,'ColorOrder') ; ColorSpecTable(end-1, :) = [0 0 0]; ColorSpecTable(end, :)=[1 1 0];
+    MarkerTable={'+','o','x','*', 's', 'd'};
+    sigDict= zeros( length(mzAxis), outNum);
+    cntSigDict = 1;
+    sigM = zeros( outNum, 1 );
+    for j = idxIns2
+    %     if ins2(j) < thresDiffW
+    %         continue;
+    %     end
+        fprintf( 'output MW: %g, max intensity of W: %g\n', SpeciesM( targetW(j) ), full( ins( targetW(j) ) ) );
+        sigM(cntSigDict) = SpeciesM( targetW(j) ) ;
+        figure('Visible','Off'); 
+        imagesc( reshape( outW(targetW(j),:), IHEI, IWID ) ); axis off; export_fig(MSFilePath);
+        [~, ~, ~, ~ ] = image_registered( bioFilePath, macFilePath, MSFilePath, requireInfoPath.registeredPath, 0.2, 0 );
+        export_fig(mapMSFilePath);
+        delete(MSFilePath);
+        figure('Visible','Off');
+        h1 = subplot(1,2,1);
+        possIdx = find(DTemplate(:,targetW(j))~=0);
+        mzRange = (mzAxis(possIdx(1))-10):(mzAxis(possIdx(end))+10);
+        h=gca;
+        plot(h, mzRange,zeros(length(mzRange), 1));
+        ylim([0 1.1]);
+        lineHan = [];
+        textCell=[];
+        cnt = 1;
+        sigDict(:, cntSigDict) = outD(:, targetW(j) ); 
+        cntSigDict = cntSigDict + 1;
+        for i = 1:length(possIdx)
+            cColor = ColorSpecTable( mod(i, 7)+1,: );
+            cLine = LineStyleTable{ mod(i, 3)+1 };
+            if outD(possIdx(i), targetW(j) ) >=1e-2
+                lineHan(cnt) = ...
+                    line( [mzAxis(possIdx(i)) mzAxis(possIdx(i))], [0 outD(possIdx(i), targetW(j) )], 'LineWidth',2,  'LineStyle', cLine, 'Color', cColor);
+                textCell{cnt} = strcat('[', deblank(DIonName{targetW(j)}{i,:}) ,']: ', num2str(mzAxis(possIdx(i))) );
+                cnt = cnt + 1;
+    %         if outD(possIdx(i), targetW(j) ) >=1e-2
+    %             annoText = strcat('[', deblank(DIonName{targetW(j)}(i,:)) ,']');
+    %             if outD(possIdx(i), targetW(j) ) >= 0.6
+    %                 dist = sqrt(length(annoText)^2/2)*0.5;
+    %                 text( mzAxis(possIdx(i))-3, outD(possIdx(i), targetW(j))-dist, annoText, 'FontSize', 9, 'Rotation',90);
+    %             else
+    %                 text( mzAxis(possIdx(i)), outD(possIdx(i), targetW(j)), annoText, 'FontSize', 9, 'Rotation', 90);
+    %             end
+    %         end
+            end
         end
+        legend(lineHan, textCell);
+        h=title( ['Inferred M:', num2str(SpeciesM(targetW(j)))]);
+        xlabel( 'mzValue' ); ylabel( 'intensity' );
+    %     set(gca, 'ytick', sort(y(y~=0)));
+        % set(gca, 'xtick', x(label(1):label(end)));
+        h2 = subplot(1,2,2, 'align');
+        tmp = imread(mapMSFilePath); imshow(tmp);colorbar;
+        ax=get(h2,'Position'); set(h2,'position',[ax(1) ax(2)-0.05 ax(3)+0.1 ax(4)+0.1]);colorbar;
+        h = title( 'The distribution in the space.');
+
+        xlabel( 'width' ); ylabel( 'height' );
+        axis([h1 h2],'square')
+        hhh=gcf; set(hhh,'pos', [1 41 1366 650] );
+        export_fig(['M_', num2str(round(SpeciesM(targetW(j)))), '_DictionaryElement.jpg']);
+        %hwplotprep
+    %     orient landscape;
+        %print ( '-dpdf', ['M_', num2str(round(SpeciesM(targetW(j)))), '_DictionaryElement'] );
+        delete(mapMSFilePath);
     end
-    legend(lineHan, textCell);
-    h=title( ['Inferred M:', num2str(SpeciesM(targetW(j)))]);
-    xlabel( 'mzValue' ); ylabel( 'intensity' );
-%     set(gca, 'ytick', sort(y(y~=0)));
-    % set(gca, 'xtick', x(label(1):label(end)));
-    h2 = subplot(1,2,2, 'align');
-    tmp = imread(mapMSFilePath); imshow(tmp);colorbar;
-    ax=get(h2,'Position'); set(h2,'position',[ax(1) ax(2)-0.05 ax(3)+0.1 ax(4)+0.1]);colorbar;
-    h = title( 'The distribution in the space.');
-
-    xlabel( 'width' ); ylabel( 'height' );
-    axis([h1 h2],'square')
-    hhh=gcf; set(hhh,'pos', [1 41 1366 650] );
-    export_fig(['M_', num2str(round(SpeciesM(targetW(j)))), '_DictionaryElement.jpg']);
-    %hwplotprep
-%     orient landscape;
-    %print ( '-dpdf', ['M_', num2str(round(SpeciesM(targetW(j)))), '_DictionaryElement'] );
-    delete(mapMSFilePath);
+else
+    sigDict= zeros( length(mzAxis), outNum);
+    cntSigDict = 1;
+    sigM = zeros( outNum, 1 );
+    for j = idxIns2
+        sigM(cntSigDict) = SpeciesM( targetW(j) ) ;
+        sigDict(:, cntSigDict) = outD(:, targetW(j) );
+        cntSigDict = cntSigDict + 1;
+    end
 end
-
 end
 
