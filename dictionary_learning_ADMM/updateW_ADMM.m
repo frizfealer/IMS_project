b42b387b13473a697365c5bda836a89de8cf3761
Yeu-Chern Harn <ycharn@csbio-desktop106.csbio.unc.edu>
2014-10-03 11:50:10 -0400

    modify input parameter, to allow output WHistCell in current folder

--- dictionary_learning_ADMM/updateW_ADMM.m
+++ dictionary_learning_ADMM/updateW_ADMM.m
@@ -1,4 +1,4 @@
-function [WResStruct] = updateW_ADMM( Y, D, W, W0, z0, z1, aMatrix, BlkDS, itNum, lambda, phi, theta, scaleFactor, logFY )
+function [WResStruct] = updateW_ADMM( Y, D, W, W0, z0, z1, aMatrix, BlkDS, itNum, lambda, phi, theta, scaleFactor, logFY, Rblk, varargin )
 
 %% variable setting
 fprintf( 'iteration number for W_update_ADMM = %d\n', itNum );
@@ -18,15 +18,6 @@ nLen = hei*wid;
 [~, mLen] = size( D );
 indMap = BlkDS.indMap;
 
-%% contructing genSparseGroupingMatrix
-[Rall] = genSparseGroupingMatrix( hei, wid, 1 );
-for i = 1:BlkDS.blkNum
-    Rblk{i} = [];
-    Rb = Rall(:, BlkDS.B2GMap{i});
-    ins = sum(abs(Rb), 2);
-    Rb(ins<2,:) = [];
-    Rblk{i} = Rb;
-end
 
 %% main optimization process
 for itNumADMM = 1:itNum
@@ -34,7 +25,7 @@ for itNumADMM = 1:itNum
     fprintf( 'LP: %g ', LPAryADMM(itNumADMM) );
     curRho = curRhoAry(itNumADMM);
 %% update W
-    fprintf( 'updating W...\t' );
+    fprintf( 'updating W...' );
     moutD = [D ones(sLen, 1)];
     tmpEye = eye(mLen+1, mLen+1);
     tmpEye(end, end) = 0;
@@ -99,7 +90,7 @@ for itNumADMM = 1:itNum
 %     [x2, funVal2]= nnLeastR( BCforUW, BD,1e-6, opts);
 %     toc;
 %% update z0
-    fprintf( 'updating z0...\t' );
+    fprintf( 'updating z0...' );
     prevZ0 = z0;    %for computing the dual residual
     parfor i = 1:nLen
         if indMap(i)
@@ -108,8 +99,7 @@ for itNumADMM = 1:itNum
     end
 
 %% update z1
-    fprintf( 'updating z1...\n' );
-    prevZ1 = z1;    %for computing the dual residual
+    fprintf( 'updating z1...\t' );
     for j = 1:BlkDS.blkNum
         loc = BlkDS.B2GMap{j};
         curW = W(:, loc);
@@ -128,76 +118,27 @@ for itNumADMM = 1:itNum
     preY = D*W(:,:) + repmat( W0(:)', sLen, 1);
     %primal residual r1
     r1 = z0 - preY;
-    %         [val, o] = max(r1(:));
-    %         fprintf( '%g %g %g\n', val,  o, full(z0(o)));
-    %         for i = 1:wid*hei
-    %             if ~indMap(i) && ~isempty( find( r1(:,i) ~= 0, 1 ) )
-    %                 fprintf( 'loc: %d', i );
-    %                 r1(:, i) = 0;
-    %             end
-    %         end
     u0 = u0 + curRho*r1;
     %compute the primal residual all
     rf = r1;
     r2 = z1 - W;
-    %         for i = 1:wid*hei
-    %             if ~indMap(i) && ~isempty( find( r2(:,i) ~= 0, 1 ) )
-    %                fprintf( 'loc: %d', i );
-    %                 r2(:, i) = 0;
-    %             end
-    %         end
-    %
-    %         fprintf( 'r2 < 0 : %d\n', length(find(r2b(:)<0)) );
     u1 = u1 + curRho* r2;
     rf = [rf; r2];
     rfn2 = norm(rf(:), 2);
 
     %compute the dual variable to see if it is converge
-    % %         dualVar = [u0; u1{1}];
-    % %         dualVar = norm(dualVar(:), 2);
-    %
     %dual residual s0
     s0 = curRho*( D'*( z0 - prevZ0 ) );
-    % %         for i = 1:wid*hei
-    % %             if ~indMap(i) && ~isempty( find( s0(:,i) ~= 0, 1 ) )
-    % %                 fprintf( 'loc: %d', i );
-    % %                 s0(:,i) = 0;
-    % %             end
-    % %         end
     s0 = [s0; sum( z0 -prevZ0, 1 )];
     s0n2 = norm( s0(:), 2 );
-%
-%         %compute the more dual residual values needed at the block condtion
-% %         duResMore = [];
-% %         for i = 1:hei*wid
-% %             bID = BlkDS.mapW2B{i};
-% %             if length(bID) > 1
-% %                 tmp = 0;
-% %                 for j = 2:length(bID)
-% %                     tmp = tmp + curRho*( ( z1{bID(j)}(:, i)- prevZ1{bID(j)}(:, i) ) );
-% %                 end
-% %                 duResMore = [duResMore; tmp];
-% %             end
-% %         end
-% %         duResMore = norm(duResMore, 2);
-% 
-%         %converge condition
-%         %primal residual condition
+    
     EPS_ABS = 1e-4;
     EPS_REL = 1e-3;
-%         
+      
     epsPri = sqrt( size(rf, 1)*size(rf, 2) )*EPS_ABS;
     relEpsPri = max( [norm( z0(:), 2 ) norm( preY(:) ) norm( z1(:), 2 ), norm( W(:), 2 )] );
     epsPri = epsPri + relEpsPri*EPS_REL;
-%         
-        %dual residual condition
-%         for i = 1:BlkDS.blkNum
-%             if dataMask(i) == 0
-%                 continue;
-%             else
-%                 tmp = tmp + u1{i};
-%             end
-%         end
+
     tmp = D'*u0;
     tmp = [tmp; sum(u0)];
     maxRel = max( norm( tmp(:), 2 ), norm( u1(:), 2 ) );
@@ -205,11 +146,13 @@ for itNumADMM = 1:itNum
     
     resRecAry(itNumADMM, :) = [rfn2, epsPri, s0n2, epsDual];
     tmp = max( abs( W(:) -  prevW_ADMM(:) ) );
-    fprintf( '%d: %g %g %g %g %g %g\n',itNumADMM, rfn2, epsPri, s0n2, epsDual, curRhoAry(itNumADMM), full(tmp) );
+    fprintf( '%d: %g %g %g %g %g %g ',itNumADMM, rfn2, epsPri, s0n2, epsDual, curRhoAry(itNumADMM), full(tmp) );
+    time = toc;
+    fprintf( 'time: %g\n', time );
     LPAryADMM(itNumADMM+1) = LP_DL_Poiss( aMatrix, Y, W, W0, D, lambda, phi, theta, scaleFactor, logFY, 0 );
     
-    if ( rfn2 < epsPri && s0n2 < epsDual ) ||...
-            ( max( abs(rf(:)) ) < 1e-3 && max( abs(s0(:)) ) < 1e-3 ) || ...
+    if ( rfn2 < epsPri && s0n2 < epsDual ) &&...
+            ( max( abs(rf(:)) ) < 1e-3 && max( abs(s0(:)) ) < 1e-3 ) && ...
             ( tmp < 1e-3 )
         break;
     end
@@ -217,23 +160,30 @@ for itNumADMM = 1:itNum
     %update the next stage rho
     if rfn2 > 10*s0n2
         curRhoAry(itNumADMM+1) = 2 * curRhoAry(itNumADMM);
-        %         elseif s0n2 > 2*rfn2
-        %             curRhoAry(itNumADMM+1) = 0.677 * curRhoAry(itNumADMM);
     elseif s0n2 > 10*rfn2
         curRhoAry(itNumADMM+1) = 0.5 * curRhoAry(itNumADMM);
     else
         curRhoAry(itNumADMM+1) = curRhoAry(itNumADMM);
     end
-    toc;
-%     rhoCell{it} = curRhoAry;
-%     resRecCell{it} = resRecAry;
+    if nargin == 16 %WHistFlag == 1
+        if varargin{nargin} == 1
+            WHistCell{itNumADMM} = W;
+        end
+    end
 end
 WResStruct.W = sparse( W );
 WResStruct.W0 = sparse( W0 );
 WResStruct.z0 = sparse( z0 );
 WResStruct.z1 = sparse( z1 );
+WResStruct.u0 = sparse(u0);
+WResStruct.u1 = sparse(u1);
 WResStruct.LPAry = sparse( LPAryADMM );
 WResStruct.rhoAry = sparse( curRhoAry );
 WResStruct.resAry = sparse( resRecAry );
-WResStruct.WDiff = sparse( full(tmp) );
+WResStruct.WDiff = sparse( tmp );
+if nargin == 16 %WHistFlag == 1
+    if varargin{nargin} == 1
+        WResStruct.WHistCell = WHistCell;
+    end
+end
 end
