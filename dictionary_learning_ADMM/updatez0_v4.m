f976b9cb23b27b6bceba7e127dcdb327171401a7
YC-HARN <ycharn@cs.unc.edu>
2014-12-29 09:17:14 -0500

    updatez0_v4
    1. using minFunc function
    2. update Z0 at once

---
+++ dictionary_learning_ADMM/updatez0_v4.m
@@ -0,0 +1,84 @@
+function [ z0 ] = updatez0_v4( alphaFlag, Y, Z0_init, D, W, W0, U0, rho, scaleFac, varargin  )
+%updatez0_i,j using minfunc function to implement
+%y (location i, j) [s 1], z0 (location i, j) [s 1], D[s m] 
+%w (location i, j) [m 1], w0 (location i, j) scalar, u0 (location i, j)[s 1]
+%[ val ] = z0_i_j_termTest( y, z0, D, w, w0, u0, rho )
+
+[sLen, nLen] = size(Y);
+res1 = - D*W - repmat( W0(:)', sLen, 1 ) + 1/rho*U0;
+if alphaFlag == 0
+    z0 = -res1;
+elseif alphaFlag == 1
+    Y = Y(:);
+    res1 = res1(:);
+    coordXY = 1:sLen*nLen;
+    vNum = sLen*nLen;
+    targetFunc = @(Z0) Z0_termFunc( Y, Z0, rho, res1, scaleFac, vNum, coordXY );
+    if ~isempty( Z0_init )
+        Z0Start = Z0_init(:);
+    else
+        Z0Start = 1e-6*zeros( sLen*nLen, 1 );
+        %     oset = D*w + w0 - 1/rho*u0;
+        %     [B,~]= lassoglm( eye(sLen), y,'poisson', 'Offset', oset, 'Alpha', 1e-8, 'Lambda', rho, 'RelTol', 1e-8 );
+        %     z0 = B+oset;
+        %     z0Start = z0; z0Start(z0Start<0) = 1e-8;
+    end
+    if length(varargin) == 1
+        itNum = varargin{1};
+    else
+        itNum = 1000;
+    end
+    if length(varargin) == 2
+        options.Method = varargin{2};
+    end
+%     lowerB = zeros(sLen,1);
+%     upperB = zeros(sLen,1); upperB(:) = Inf;
+%     options = optimoptions('fminunc');
+%     options.MaxFunEvals= 1e6;
+%     options.MaxIter = itNum;
+%     options.TolFun=1e-6;
+%     options.TolX=1e-6;
+%     options.Algorithm='trust-region';
+%     options.Display='none';
+%     options.Hessian='on';
+% %     options.Diagnostics = 'off';
+% %     options.HessUpdate='steepdesc';
+%      % options.UseParallel='always';
+%     options.GradObj='on';
+        options.Display = 'off';
+%     [ z0, ~ ] = fminunc( targetFunc, Z0Start, options );
+     [ z0, ~ ] = minFunc( targetFunc, Z0Start, options );
+    z0 = reshape( z0, sLen, nLen );
+end
+
+end
+
+function [ val,grad, H ] = Z0_termFunc( Y, Z0, rho, res1, scaleFac, vNum, coordXY )
+%Z0_termFunc the terms consisted of z0 in ADMM formulation
+eZ0 = exp(Z0);
+%     fprintf( 'll = %g, pp = %g\n', full(scaleFac*sum( ( -y.*z0 + eZ0 ))), full(rho / 2 * sum( (z0 +res1).^2 )));
+val = sum( scaleFac*( -Y.*Z0 + eZ0 ) ) + rho / 2 * sum( (Z0 +res1).^2 );
+grad = scaleFac*(-Y + eZ0) + rho* ( Z0 + res1 );
+tmp = scaleFac*eZ0 + rho;
+H = sparse( coordXY, coordXY, tmp, vNum, vNum );
+end
+
+function [ hMat ] = z0_i_j_HessianFFunc( z0, lambda, rho  )
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+sLen = size(z0, 1);
+eZ0 = exp(z0);
+tmp = eZ0 + rho;
+hTmp = zeros( sLen, 1);
+for i = 1:sLen
+    hTmp(i) = tmp(i);
+end
+idx = 1:sLen;
+hMat = sparse( idx, idx, hTmp, sLen, sLen );
+% for i = 1:sLen
+%     hMat(i,i) = tmp(i);
+% end
+% hMat = sparse(hMat);
+end
+
+
---
+++ testFunctions/test_updatez0_v4.m
@@ -0,0 +1,113 @@
+function [  ] = test_updatez0_v4( testCaseNum )
+%test_updatez0_v4 test function for updatez0_v4
+if testCaseNum == 1
+    %% synthesize data first
+    SLEN = 10;
+    MLEN = 10;
+    HEIGHT = 20;
+    WIDTH = 20;
+    DOptions.sparsePrec = 1;
+    DOptions.coheMax = 1;
+    WOptions.supPrec = 1;
+    WOptions.sparsePrec = 1;
+    verbose = 0;
+    [simData] = synthesizeData_Poisson( SLEN, MLEN, HEIGHT, WIDTH, [], DOptions, WOptions, verbose );
+    %% test on updatez0_v4 function, no held out data
+    alpha = ones(HEIGHT, WIDTH);
+    inY = simData.gY;
+    BlkDS = conBLKDS(inY);
+    gD = simData.gD;
+    gW = simData.gW;
+    gW0 = simData.gW0;
+    U0 = zeros( SLEN, HEIGHT*WIDTH );
+    curRho = 1;
+    scaleFactor = 1e-8;
+    Z0 = zeros( SLEN, HEIGHT*WIDTH );
+    funcVal(1) = Z0_termFunc( inY, Z0, curRho, gD, gW, gW0, U0, scaleFactor );
+    for j = 1:BlkDS.blkNum
+            loc = BlkDS.B2GMap{j};
+            locAlpha1 = intersect( loc, find( alpha(:) == 1 ) );
+            %update for training data
+            curY = inY(:, locAlpha1);
+            curW = gW(:, locAlpha1);
+            curW0 = gW0(locAlpha1);
+            curZ0 = Z0(:, locAlpha1);
+            curU0 = U0(:, locAlpha1);
+            [ tmpZ0 ] = updatez0_v4( 1, curY, curZ0, gD, curW, curW0, curU0, curRho, scaleFactor );
+            Z0(:, locAlpha1) = tmpZ0;
+            funcVal(j+1) = Z0_termFunc( inY, Z0, curRho, gD, gW, gW0, U0, scaleFactor );
+    end
+    funcVal
+    statMap = zeros( SLEN, HEIGHT*WIDTH );
+    for j = 1:BlkDS.blkNum
+        loc = BlkDS.B2GMap{j};
+        tmp = abs( (Z0(:, loc)) - log(inY(:, loc)) );
+        statMap(:, loc) = tmp;
+    end
+    max(max(statMap))
+    figure;
+    for i = 1:SLEN
+        subplot( 2, 5, i ); imagesc( reshape( statMap(i, :), HEIGHT, WIDTH ) );
+    end
+elseif testCaseNum == 2
+    load( 'D:\Google ¶³ºÝµwºÐ\realExp_100831_348_136_12_40hr_0-1XLB_LN\100831_348_136_12_40hr_0_1XLB_LN_input.mat' );
+    load( 'D:\Google ¶³ºÝµwºÐ\realExp_100831_348_136_12_40hr_0-1XLB_LN\exp_100831_348_136_12_40hr_0_1XLB_LN_4_res.mat' );
+    method = {'csd', 'bb', 'pnewton0', 'scg', 'pcg', 'lbfgs', 'newton'};
+    for i = 1:7
+        fprintf( '%s\n', method{i});
+        inY = dataCube(:, :);
+        Z0 = zeros( size( expRec.z0 ) );
+        curRho = 1;
+        gD = expRec.outD;
+        gW = expRec.outW;
+        gW0 = expRec.outW0;
+        U0 = zeros( size(inY, 1), size(inY, 2)*size(inY, 3) );
+        scaleFactor = 1;
+        funcVal(1) = Z0_termFunc( inY, Z0, curRho, gD, gW, gW0, U0, scaleFactor );
+        alpha = ones( size(inY, 2), size(inY, 3) );
+        tic
+        for j = 1:BlkDS.blkNum
+            loc = BlkDS.B2GMap{j};
+            locAlpha1 = intersect( loc, find( alpha(:) == 1 ) );
+            %update for training data
+            curY = inY(:, locAlpha1);
+            curW = gW(:, locAlpha1);
+            curW0 = gW0(locAlpha1);
+            curZ0 = Z0(:, locAlpha1);
+            curU0 = U0(:, locAlpha1);
+            [ tmpZ0 ] = updatez0_v4( 1, curY, curZ0, gD, curW, curW0, curU0, curRho, scaleFactor, 1000, method{i} );
+            Z0(:, locAlpha1) = tmpZ0;
+            funcVal(j+1) = Z0_termFunc( inY, Z0, curRho, gD, gW, gW0, U0, scaleFactor );
+        end
+        toc
+        funcVal
+    end
+elseif testCaseNum == 3
+    load( 'D:\Google ¶³ºÝµwºÐ\realExp_100831_348_136_12_40hr_0-1XLB_LN\100831_348_136_12_40hr_0_1XLB_LN_input.mat' );
+    load( 'D:\Google ¶³ºÝµwºÐ\realExp_100831_348_136_12_40hr_0-1XLB_LN\exp_100831_348_136_12_40hr_0_1XLB_LN_4_res.mat' );
+    inY = dataCube(:, :);
+    Z0 = zeros( size( expRec.z0 ) );
+    curRho = 1;
+    gD = expRec.outD;
+    gW = expRec.outW;
+    gW0 = expRec.outW0;
+    U0 = zeros( size(inY, 1), size(inY, 2)*size(inY, 3) );
+    scaleFactor = 1;
+    funcVal(1) = Z0_termFunc( inY, Z0, curRho, gD, gW, gW0, U0, scaleFactor );
+    alpha = ones( size(inY, 2), size(inY, 3) );
+    tic
+    for i = 1:size(inY, 2)*size(inY, 3)
+        [ Z0(:, i) ] = updatez0_v3( alpha(i), inY(:,i), Z0(:,i), gD, gW(:, i), gW0(i), U0(:,i), curRho, scaleFactor );
+    end
+    toc
+    funcVal(2) = Z0_termFunc( inY, Z0, curRho, gD, gW, gW0, U0, scaleFactor );
+end
+
+end
+
+function [ val ] = Z0_termFunc( inY, Z0, rho, gD, gW, gW0, U0, scaleFac )
+res1 = -gD*gW(:, :) - repmat( gW0(:)', size(gD, 1), 1 ) + 1/rho*U0;
+eZ0 = exp(Z0);
+%     fprintf( 'll = %g, pp = %g\n', full(scaleFac*sum( ( -y.*z0 + eZ0 ))), full(rho / 2 * sum( (z0 +res1).^2 )));
+val = sum( sum( scaleFac*( -inY(:, :).*Z0 + eZ0 ) ) ) + rho / 2 * sum( sum( (Z0 +res1).^2 ) );
+end
