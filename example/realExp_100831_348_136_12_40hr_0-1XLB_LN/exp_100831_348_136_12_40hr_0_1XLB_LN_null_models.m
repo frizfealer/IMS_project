2a54e941ed04c95258448b8d1f3105bb57bec04d
YC-HARN <ycharn@cs.unc.edu>
2015-01-01 23:17:53 -0500

    script for compute null model
    compute null model (hyperparameters=0) for
    100831_348_136_12_40hr_0_1XLB_LN

---
+++ example/realExp_100831_348_136_12_40hr_0-1XLB_LN/exp_100831_348_136_12_40hr_0_1XLB_LN_null_models.m
@@ -0,0 +1,9 @@
+function [ expRec, gridVec ] = exp_100831_348_136_12_40hr_0_1XLB_LN_null_models()
+%% loading variables from exp 1
+load( '100831_348_136_12_40hr_0_1XLB_LN_input_20141231.mat' );
+[ param ] = setDLParameters();
+lambda = 1e-32; theta = 1e-32; phi = 1e-32;
+[ expRec ] = dictionaryLearning_ADMM_v6( dataCube, [], nDTemplate, [], lambda, theta, phi, aMatrix, BlkDS, [], 'temp.mat', param );
+%%
+
+end
