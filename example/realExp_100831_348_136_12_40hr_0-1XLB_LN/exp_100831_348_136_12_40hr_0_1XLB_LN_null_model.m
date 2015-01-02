a6f60f2fbcc780ef59837a68dd3f96e0f29c5a7f
YC-HARN <ycharn@cs.unc.edu>
2015-01-01 23:35:22 -0500

    Update script
    change the name of null model script

--- example/realExp_100831_348_136_12_40hr_0-1XLB_LN/exp_100831_348_136_12_40hr_0_1XLB_LN_null_model.m
+++ example/realExp_100831_348_136_12_40hr_0-1XLB_LN/exp_100831_348_136_12_40hr_0_1XLB_LN_null_model.m
@@ -1,4 +1,4 @@
-function [ expRec, gridVec ] = exp_100831_348_136_12_40hr_0_1XLB_LN_null_models()
+function [ expRec, gridVec ] = exp_100831_348_136_12_40hr_0_1XLB_LN_null_model()
 %% loading variables from exp 1
 load( '100831_348_136_12_40hr_0_1XLB_LN_input_20141231.mat' );
 [ param ] = setDLParameters();
