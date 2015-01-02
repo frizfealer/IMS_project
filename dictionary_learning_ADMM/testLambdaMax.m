0619fd490b5ad569b150a4e2ee2d2e178f60d653
YC-HARN <ycharn@cs.unc.edu>
2015-01-01 23:36:05 -0500

    updatefunction testLambdaMax
    1. save results every lambda
    2. change to not using parfor

---
+++ dictionary_learning_ADMM/testLambdaMax.m
@@ -0,0 +1,34 @@
+function [ dfVec, lambdaVec ] = testLambdaMax( Y, D, startVal, intVal, intThres, testNum, seq, w_tol )
+%testLambdaLimit test lambda max
+if ~isempty(seq)
+    lambdaVec = seq;
+else
+    endVal = startVal+testNum*intVal;
+    lambdaVec = startVal:intVal:endVal;
+end
+dfVec = zeros(length(lambdaVec), 1);
+for i = 1:length(lambdaVec)
+    lambda = lambdaVec(i);
+    fprintf( 'lambda = %g\n', lambda );
+    [~, hei, wid] = size(Y);
+    aMatrix = ones( hei, wid );
+    itNum = 100;
+    theta = 1e-32;
+    L1Flag = 1;
+    logFY = [];
+    initVar = [];
+    [WResStruct] = updateW_ADMM_v3( Y, D, aMatrix, itNum, lambda, theta, L1Flag, logFY, initVar, 0, w_tol );
+    BlkDS = conBLKDS( Y );
+    %figure; subplot(1, 2, 1); plot(WResStruct.LPAry(:, 1)); subplot(1, 2, 2); plot(WResStruct.LPAry(:, 2) );
+    ins = WResStruct.W(:, BlkDS.indMap == 1 );
+    tmp = zeros( size(ins, 1), 1);
+    for j = 1:size(ins, 1);
+        tmp(j) = max(ins(j, :));
+    end
+    figure;plot(tmp)
+    dfVec(i) = length(find(tmp>intThres));
+    save( 'testLambdaMax_temp.mat', 'dfVec', 'lambdaVec' );
+end
+
+end
+
