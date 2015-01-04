f9076a86035e2c44e89bfc124e6b453b5deac7b7
YC-HARN <ycharn@cs.unc.edu>
2015-01-04 11:59:31 -0500

    exec_AllScriptsInFolder
    execute all scripts in a folder, a python script

---
+++ scripts/exec_AllScriptsInFolder.py
@@ -0,0 +1,26 @@
+import fnmatch
+import os
+import sys
+import subprocess
+import re
+import time
+#inPath = str(sys.argv[1]);
+#print 'Input path folder is: %s' % (inPath)
+#os.chdir(inPath)
+for root, dirnames, filenames in os.walk(os.getcwd()):
+	print 'Get all files in this folder...'
+[tmp, folderName] = os.path.split(os.getcwd())
+print folderName
+logFolderName = folderName+'_logs'
+logFNPath = '../'+logFolderName+'/'
+if not os.path.exists(logFNPath):
+    os.makedirs(logFNPath)
+for file in filenames:
+	#cmd = 'matlab -q day -nodesktop -nodisplay -nosplash -r "' + file[:-2] + '; exit" -logfile ' + file[:-2]+'.log &'
+	cmd = 'bsub matlab -nodisplay -nosplash -singleCompThread -r ' + file[:-2] + ' -logfile ' + logFNPath + file[:-2]+'.log';
+
+	print cmd
+	#subprocess.call(cmd, shell=False)
+	#subprocess.Popen(cmd, cwd = inPath);
+	#os.system( cmd )
+	time.sleep(1)
