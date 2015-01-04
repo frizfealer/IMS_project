baeb8ad93cc92aa76871ad36c8b6454691380272
YC-HARN <ycharn@cs.unc.edu>
2015-01-04 12:01:04 -0500

    Update function testLambdaMaxL2
    use parfor for this function

--- dictionary_learning_analysis/testLambdaMaxL2.m
+++ dictionary_learning_analysis/testLambdaMaxL2.m
@@ -7,7 +7,7 @@ else
     lambdaVec = startVal:intVal:endVal;
 end
 devVec = zeros(length(lambdaVec), 1);
-for i = 1:length(lambdaVec)
+parfor i = 1:length(lambdaVec)
     lambda = lambdaVec(i);
     fprintf( 'lambda = %g\n', lambda );
     [~, hei, wid] = size(Y);
@@ -22,21 +22,21 @@ for i = 1:length(lambdaVec)
     BlkDS = conBLKDS( Y );
     %figure; subplot(1, 2, 1); plot(WResStruct.LPAry(:, 1)); subplot(1, 2, 2); plot(WResStruct.LPAry(:, 2) );
     ins = WResStruct.W(:, BlkDS.indMap == 1 );
-    tmp = zeros( size(ins, 1), 1);
-    for j = 1:size(ins, 1);
-        tmp(j) = mean(ins(j, :));
-    end
+    tmp = ins(:);
     %figure;plot(tmp)
     thres = 0.66*length(tmp);
     [counts, centers] = hist(tmp, 1000);
     accCnt = 0;
+    outJ = 1;
+    Flag = 0;
     for j = 1:1000
         accCnt = accCnt + counts(j);
-        if accCnt >= thres
-            break;
+        if Flag == 0 && accCnt >= thres 
+            outJ = j;
+            Flag = 1;
         end
     end
-    devVec(i) = centers(j);
+    devVec(i) = centers(outJ);
     %figure; hist(tmp, 1000);
 %     dfVec(i) = length(find(tmp>intThres));
     %save( 'testLambdaMax_temp.mat', 'dfVec', 'lambdaVec' );
