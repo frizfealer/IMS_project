59b8badaac0d2aa6a5c158a8ab18a5ce0f3d6174
YC-HARN <ycharn@cs.unc.edu>
2015-01-04 11:52:07 -0500

    genAll_hypers_grid_searchScript
    scripts to generate scripts for all hyper-parameters grid search with L1
    and L2 penalties.

--- scripts/genAll_hypers_grid_searchScript_L2.m
+++ scripts/genAll_hypers_grid_searchScript_L2.m
@@ -1,17 +1,17 @@
-function [ ] = genAll_hypers_grid_searchScript( folderName )
+function [ ] = genAll_hypers_grid_searchScript_L2( folderName )
 %genTestLambdaScript generate TestLambdaScript for each seq
 mkdir(folderName);
 cd(folderName);
 seq = 1:125;
 for i = 1:length(seq)
-    fileName = ['all_hypers_grid_searchScript_' num2str(i) '_.m'];
+    fileName = ['all_hypers_grid_search_L2Script_' num2str(i) '_.m'];
     fileID = fopen(fileName, 'w+');
     fprintf( fileID, 'addpath( genpath( ''~/IMS_project-master'' ) );\n' );
     fprintf( fileID, 'addpath( genpath( ''~/glmnet_matlab'' ) );\n' );
     fprintf( fileID, 'addpath( genpath( ''~/Ipopt_3118'' ) );\n' );
     fprintf( fileID, 'addpath( genpath( ''~/SLEP_package_4.1'' ) );\n' );
     fprintf( fileID, 'addpath( genpath( ''~/minFunc_2012'' ) );\n' );
-    fprintf( fileID, ['exp_100831_348_136_12_40hr_0_1XLB_LN_all_hypers_grid_search( ' num2str(i) ' );\n'] );
+    fprintf( fileID, ['exp_100831_348_136_12_40hr_0_1XLB_LN_all_hypers_grid_search_L2( ' num2str(i) ' );\n'] );
     fclose(fileID);
 end
 
