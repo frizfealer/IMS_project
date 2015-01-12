ac7673875cbb7237309a2c80527c0287fdefe525
YC-HARN <ycharn@cs.unc.edu>
2015-01-11 23:21:35 -0500

    delete scripts

--- example/realExp_100831_348_136_12_40hr_0-1XLB_LN/two_hyper_grid_searchScripts/two_hypers_grid_search_script_18_.m
+++
@@ -1,6 +0,0 @@
-addpath( genpath( '~/IMS_project-master' ) );
-addpath( genpath( '~/glmnet_matlab' ) );
-addpath( genpath( '~/Ipopt_3118' ) );
-addpath( genpath( '~/SLEP_package_4.1' ) );
-addpath( genpath( '~/minFunc_2012' ) );
-exp_100831_348_136_12_40hr_0_1XLB_LN_two_hypers_grid_search( 18 );
