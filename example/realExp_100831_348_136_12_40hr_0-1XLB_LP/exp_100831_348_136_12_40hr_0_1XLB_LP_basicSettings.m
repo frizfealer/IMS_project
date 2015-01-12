c55d8d9f135cfa7fff825bc37eebd03c9fc85c74
YC-HARN <ycharn@cs.unc.edu>
2015-01-11 22:23:21 -0500

    update environment var. and scripts for real exp.
    1. include a basicSetting script
    2. include environment variables

---
+++ example/realExp_100831_348_136_12_40hr_0-1XLB_LP/exp_100831_348_136_12_40hr_0_1XLB_LP_basicSettings.m
@@ -0,0 +1,82 @@
+function [ expRec, gridVec ] = exp_100831_348_136_12_40hr_0_1XLB_LP_basicSettings()
+%experiment scripts update @ 2015/01/08
+%% load data, including dataCube and mzAxis
+load( '100831_348_136_12,40hr_0-1XLB_LP.mzML_dc.mat' );
+%% generate BlkDS, a data structure for bacteria community location information
+%BlkDS has the 4 fields
+%blkNum: the bacteria community number 
+%B2GMap: a mapping from bacteria community to grid
+%G2BMap: a mapping from grid to bacteria community
+%indMap: a logical matrix, as the same size of the grid, with 1 means the
+%grids having signals, 0 means empty
+BlkDS = conBLKDS( dataCube );
+%% bin dataCube
+[ mDataCube, mMZAxis, ~ ] = binningDataCube( dataCube, mzAxis, BlkDS );
+dataCube = mDataCube; mzAxis = mMZAxis;
+%% trim data, only consdier m/z>=mzSmall and m/z <=mzLarge
+mzSmall = 500;
+mzLarge = max(mzAxis);
+assert( mzSmall >= min( mzAxis ) );
+assert( mzLarge <= max( mzAxis ) );
+% dataCube, mzAxis is loaded from the *.mat file in dataCubeFilePath
+tsIdx = find(mzAxis>=mzSmall, 1);
+teIdx = find( mzAxis<=mzLarge , 1, 'last' );
+dataCube = dataCube(tsIdx:teIdx, :, :);
+mzAxis = mzAxis(tsIdx:teIdx);
+
+%% retrieve data dimension
+[SLEN, IHEIGHT, IWIDTH] = size( dataCube );
+
+%% trim data, only consider m/z channel has values larger then the value of  intThreshold accross all the grids
+intThreshold = 100;
+ins = dataCube(:,:);
+ins = ins(:, BlkDS.indMap);
+tmp = zeros( size( ins, 1 ), 1 );
+for i = 1:length(tmp)
+    tmp(i) = max( ins(i, :) );
+end
+hist(tmp, 100);
+tIdx = find(tmp>=intThreshold);
+dataCube = dataCube(tIdx, :, :);
+mzAxis = mzAxis(tIdx);
+
+%% generate aMatrix, alpha matrix for leave-out testing.
+%if aMatrix is set to ones e.g. aMatrix = ones( IHEIGHT, IWIDTH );,
+%then there is no leave-out
+%aMatrix: a logical matrix, as the same size of the grid, with 1 means the
+%grids used in training process, and 0 means in testing process
+% aMatrix = ones( IHEIGHT, IWIDTH );
+[ aMatrix ] = leaveoutCBPatternsData( BlkDS, 10 );
+%% generate DTemplate
+IonTableFilePathPos = 'D:\Users\YeuChern\GitHub\IMS_project\example\molecule_profile_pos.csv';
+%smallest molecule weight, set to H = 1.007
+%m/z error +/- 0.5
+[ pDTemplate, pDIonName, pSpeciesM ] = genDTemplate_v2( dataCube, mzAxis, IonTableFilePathPos, 1.007, 0.5 );
+
+save( '100831_348_136_12_40hr_0_1XLB_LP_input_20150108.mat' );
+
+% %% running dictionary learning
+% lambda = 1e-6; phi = 1e-6; theta = 1e-6;
+% snapPath = 'temp_1.mat';
+% %setting up parameters
+% [ param ] = setDLParameters();
+% param.D_ION_NAME = nDIonName;
+% % param.D_HIST_PATH = 'DHist_l_0_t_0_p_0.mat';
+% % param.W_HIST_PATH = 'WHist_l_0_t_0_p_0.mat';
+% save( '100831_348_136_12_40hr_0_1XLB_LN_input.mat' );
+% 
+% %% run an estimate of hyper parameters
+% [ expRec ] = dictionaryLearning_ADMM_v5( dataCube, [], nDTemplate, [], lambda, theta, phi, aMatrix, BlkDS, [], snapPath, param );
+% save( 'exp_100831_348_136_12_40hr_0_1XLB_LN_1_res.mat', 'expRec' );
+% [ elambda, ~, ephi ] = estimateHypParam( expRec.outW, expRec.outD, nDTemplate, BlkDS );
+% clear expRec
+% [ expRec1_2 ] = dictionaryLearning_ADMM_v5( dataCube, [], nDTemplate, [], elambda, theta, ephi, aMatrix, BlkDS, [], snapPath, param );
+% save( 'exp_100831_348_136_12_40hr_0_1XLB_LN_1_2_res.mat', 'expRec1_2' );
+% [ ~, etheta, ~ ] = estimateHypParam( expRec1_2.outW, expRec1_2.outD, nDTemplate, BlkDS );
+% clear expRec1_2
+% save( '100831_348_136_12_40hr_0_1XLB_LN_input.mat' );
+% %%
+
+end
+
+
