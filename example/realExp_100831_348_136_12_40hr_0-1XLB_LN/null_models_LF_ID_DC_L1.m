95ad1b200deda5fe571d4f6f48d1585459d202ee
YC-HARN <ycharn@cs.unc.edu>
2015-01-25 22:29:45 -0500

    two new experiment
    1. on new dictionary template
    2. with L1 constraints on dictionary
    3. LF_ID: identity link function
    4. LF_log: log link function

---
+++ example/realExp_100831_348_136_12_40hr_0-1XLB_LN/null_models_LF_ID_DC_L1.m
@@ -0,0 +1,16 @@
+function [ ] = null_models_LF_ID_DC_L1()
+addpath( genpath( '~/IMS_project-master' ) );
+addpath( genpath( '~/glmnet_matlab' ) );
+addpath( genpath( '~/Ipopt_3118' ) );
+addpath( genpath( '~/SLEP_package_4.1' ) );
+addpath( genpath( '~/minFunc_2012' ) );
+%% loading variables from exp 1
+load( '100831_348_136_12_40hr_0_1XLB_LN_input_20141231.mat' );
+[ param ] = setDLParameters();
+lambda = 1e-32; theta = 1e-32; phi = 1e-32;
+param.D_CONSTRAINTS = 'L1';
+param.linkFunc = 'identity';
+[ expRec ] = dictionaryLearning_ADMM_v6( dataCube, [], DTemplate3, [], lambda, theta, phi, aMatrix, BlkDS, [], 'temp1.mat', param );
+%%
+save( 'null_model_20150111_lf_iden_DC_L1.mat', 'expRec' );
+end
---
+++ example/realExp_100831_348_136_12_40hr_0-1XLB_LN/null_models_LF_log_DC_L1.m
@@ -0,0 +1,16 @@
+function [ ] = null_models_LF_log_DC_L1()
+addpath( genpath( '~/IMS_project-master' ) );
+addpath( genpath( '~/glmnet_matlab' ) );
+addpath( genpath( '~/Ipopt_3118' ) );
+addpath( genpath( '~/SLEP_package_4.1' ) );
+addpath( genpath( '~/minFunc_2012' ) );
+%% loading variables from exp 1
+load( '100831_348_136_12_40hr_0_1XLB_LN_input_20141231.mat' );
+[ param ] = setDLParameters();
+lambda = 1e-32; theta = 1e-32; phi = 0;
+param.D_CONSTRAINTS = 'L1';
+param.linkFunc = 'log';
+[ expRec ] = dictionaryLearning_ADMM_v6( dataCube, [], DTemplate3, [], lambda, theta, phi, aMatrix, BlkDS, [], 'temp2.mat', param );
+%%
+save( 'null_model_20150111_lf_log_DC_L1.mat', 'expRec' );
+end
