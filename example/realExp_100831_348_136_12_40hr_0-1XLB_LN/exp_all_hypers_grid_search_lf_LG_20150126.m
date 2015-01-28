686f303852b692c3bf31601b53c3eeb158ba7476
YC-HARN <ycharn@cs.unc.edu>
2015-01-28 11:05:34 -0500

    experiment script for LF log gauss

---
+++ example/realExp_100831_348_136_12_40hr_0-1XLB_LN/exp_all_hypers_grid_search_lf_LG_20150126.m
@@ -0,0 +1,16 @@
+function [ expRec, gridVec ] = exp_all_hypers_grid_search_lf_LG_20150126( index )
+%% loading variables from exp 1
+load( '~/IMS_project-master/example/realExp_100831_348_136_12_40hr_0-1XLB_LN/100831_348_136_12_40hr_0_1XLB_LN_input_20141231.mat' );
+% load('D:\Users\YeuChern\GitHub\IMS_project\example\realExp_100831_348_136_12_40hr_0-1XLB_LN/100831_348_136_12_40hr_0_1XLB_LN_input_20141231.mat');
+[ param ] = setDLParameters();
+param.linkFunc = 'log_gaussain';
+[lVec, tVec] = meshgrid( lambdaVec_lf_LG...
+    , thetaVec_lf_LG );
+lambda = lVec(index); theta = tVec(index);
+tempName = ['exp_100831_348_136_12_40hr_0_1XLB_LN_tmp_' num2str(index) '_.mat' ];
+fprintf( 'ready to run!\n' );
+[ expRec ] = dictionaryLearning_ADMM_v6( dataCube, [], DTemplate3, [], lambda, theta, 1e-32, aMatrix, BlkDS, [], tempName, param );
+mkdir( '/lustre/scr/y/h/yharn/all_hypers_grid_search_results_lf_LG_20150126' );
+resName = ['exp_100831_348_136_12_40hr_0_1XLB_LN_res_' num2str(index) '_.mat' ];
+save( ['/lustre/scr/y/h/yharn/all_hypers_grid_search_results_lf_LG_20150126/' resName], 'expRec');
+
